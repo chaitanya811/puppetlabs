5c84600b2bd3aedbfdeb7137b5ec832a18f0c151
root <root@chandu8111.mylabserver.com>
2018-12-30 16:30:18 +0000

    bug fixes

--- ntp/manifests/config.pp
+++ ntp/manifests/config.pp
@@ -3,10 +3,11 @@ class ntp::config (
  String $config_file_mode = $ntp::config_file_mode,
  Array[String] $servers   = $ntp::servers,
 ) {
-  file { "/etc/${config_name}":
+  file { "/etc/${config_name}" :
     ensure  => file,
     owner   => 0,
     group   => 0,
     mode    => $config_file_mode,
     content => template("$module_name/ntp.conf.erb"),
+}
 }
--- ntp/manifests/service.pp
+++ ntp/manifests/service.pp
@@ -1,13 +1,13 @@
 class ntp::service(
  String $service_name = $ntp::service_name,
  String $service_ensure = $ntp::service_ensure,
- String $service_enable = $ntp::service_enable,
- String $service_hasstatus = $ntp::service_hasstatus,
- String $service_hasrestart = $ntp::service_hasrestart,
+ Boolean $service_enable = $ntp::service_enable,
+ Boolean $service_hasstatus = $ntp::service_hasstatus,
+ Boolean $service_hasrestart = $ntp::service_hasrestart,
 ) {
    service { 'ntp':
-     ensure     => service_ensure,
-     enable     => service_enable,
+     ensure     => $service_ensure,
+     enable     => $service_enable,
      name       => $service_name,
      hasstatus  => $service_hasstatus,
      hasrestart => $service_hasrestart, 
